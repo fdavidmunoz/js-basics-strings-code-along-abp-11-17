04d7dc865e585be333102c93a6b98f5171ec22c1
Francisco David Muñoz <fdavidmunoz@gmail.com>
2017-11-12 10:45:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'welcome, ' + currentUser + '!';
+const shortGreeting = 'welcome, ' + currentUser.charAt(0) + '!';
