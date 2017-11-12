27135159dbd66964f788695e8b0d7cb9e81fdaf8
Francisco David Muñoz <fdavidmunoz@gmail.com>
2017-11-12 10:29:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = "Paco";
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
-currentUser = 'PACO';
+
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!';
